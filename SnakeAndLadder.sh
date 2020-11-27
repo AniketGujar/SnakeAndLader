03668639cf604a117d534ca3a3999a6b8c300a42
AniketGujar <gujaraniket7875@gmail.com>
2020-11-27 12:10:43 +0530

    Added:Option to find out No Paly, Ladder or Snake

--- SnakeAndLadder.sh
+++ SnakeAndLadder.sh
@@ -1,15 +1,38 @@
 #!/bin/bash
 
-function snakeladder() {
-        position=0;
+position=0
+
+function snakeLadder() {
         echo "Player added at" $position "position"
 	rollDice
 }
 
 function rollDice() {
         dice=$((RANDOM%6+1))
-        echo $dice
+
+        echo "Dice Rolled=" $dice
+	option
+	echo "Position after dice rolled=" $position
 }
 
+function option(){
+	optionDice=$((RANDOM%3+1))
 
-snakeladder
+        case $optionDice in
+        1) position=0
+	   echo "Option: No Play" ;;
+
+        2) position=$((position+$dice))
+	   echo "Option: Ladder" ;;
+
+        3) position=$((position-$dice))
+	   echo "Option: Snake"
+                if [[ position -le 0 ]]
+                then
+                   position=0
+                fi 		;;
+	esac
+}
+
+echo "Snake and Laddder Game Started"
+snakeLadder
