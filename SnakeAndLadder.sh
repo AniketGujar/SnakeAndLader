f9b1a82a6a726a91b273804ab802429977c54d6c
AniketGujar <gujaraniket7875@gmail.com>
2020-11-27 13:22:25 +0530

    Added: Condition to ensure exact winning position of 100

--- SnakeAndLadder.sh
+++ SnakeAndLadder.sh
@@ -4,28 +4,32 @@ position=0
 
 function snakeLadder() {
         echo "Player added at" $position "position"
-	rollDice
+        option
+        echo "Position after dice rolled=" $position
+
 }
 
 function rollDice() {
         dice=$((RANDOM%6+1))
-
         echo "Dice Rolled=" $dice
-	option
-	echo "Position after dice rolled=" $position
 }
 
 function option(){
 	while [[ $position -lt 100 ]]
 	do
-
+		rollDice
 		optionDice=$((RANDOM%3+1))
 
 	        case $optionDice in
         	1) echo "Option: No Play" ;;
 
 	        2) position=$((position+$dice))
-		   echo "Option: Ladder" ;;
+		   echo "Option: Ladder"
+
+			if [[ position -gt 100 ]]
+			then
+				position=$((position-$dice))
+			fi		 ;;
 
 	        3) position=$((position-$dice))
 		   echo "Option: Snake"
@@ -35,6 +39,7 @@ function option(){
                 	   position=0
                 	fi 		;;
 		esac
+	echo "Position = "$position
 	done
 
 }
