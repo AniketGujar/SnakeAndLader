735463814b9af5235a01be82856dfa2c761e400e
AniketGujar <gujaraniket7875@gmail.com>
2020-11-27 10:54:18 +0530

    Added: Dice Rolling using Random

--- SnakeAndLadder.sh
+++ SnakeAndLadder.sh
@@ -3,4 +3,13 @@
 function snakeladder() {
         position=0;
         echo "Player added at" $position "position"
+	rollDice
 }
+
+function rollDice() {
+        dice=$((RANDOM%6+1))
+        echo $dice
+}
+
+
+snakeladder
